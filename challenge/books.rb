760d94ec2393caf3f652571c223167a63c945863
Bryce Wein <bryce.wein@gmail.com>
2022-01-12 22:58:27 -0500

    Edit Verbage

--- challenge/books.rb
+++ challenge/books.rb
@@ -1,12 +1,12 @@
 # The name was chosen because the file is about books!
 title = "Extreme Ownership"
-# Assigned the variable "length" the value of 540
+# Assign the variable "length" the value of 540
 length = 540
-# Assigned the variable "cover_color" the value of "red"
+# Assign the variable "cover_color" the value of "red"
 cover_color = "red"
-# Assigned the variable "good_read" the value of true
+# Assign the variable "good_read" the value of true
 good_read = true
-# Assigned the variable read_time the value of 10.5
+# Assign the variable read_time the value of 10.5
 read_time = 10.5
 has_read = false
 
